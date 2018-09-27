5d66bf40e2f7db9c019898eaa06234e80931fe47
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-09-27 16:40:44 +0530

    completed the project

---
+++ src/components/BookForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form v-on:submit.prevent="bookSubmit(bookTitle,bookAuthor)" action="#" method="post">
+        <input v-model="bookTitle" type="text" placeholder="Enter Book:">
+        <input v-model="bookAuthor" type="text" placeholder="Enter Author's name:">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
+
+<script>
+
+    export default {
+        name: 'BookForm',
+        props:['books'],
+        data(){
+            return{
+                bookTitle : '' ,
+                bookAuthor : ''
+            }
+        },
+        methods:{
+            bookSubmit(bookTitle,bookAuthor){
+                this.$emit('addbook',bookTitle,bookAuthor)
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
