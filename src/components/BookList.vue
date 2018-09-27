588f8c8cee54dcbe1a7fc0c83b9d0bbdad5412a7
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-09-27 15:22:05 +0530

    completed module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
-        <ul><li v-for="book in books"> </li></ul>
+        <ul>
+            <book-item v-for="book in books" :book="book"></book-item>
+        </ul>
     </div>
 </template>
 
 <script>
+import BookItem from './BookItem'; 
 export default {
     name : 'BookList',
+    components : {
+        BookItem 
+    },
     data() {
         return{
             title : 'All Books',
-            books :[{title:'Self-Reliance',author:'Ralph Waldo Emerson'},
-                    {title:'American Gods',author:'Neil Gaiman'},
-                    {title:'Amusing Ourselves to Death',author:'Neil Postman'}]
+            books :[
+                { title:'Self-Reliance', author:'Ralph Waldo Emerson'},
+                { title:'American Gods', author:'Neil Gaiman'},
+                { title:'Amusing Ourselves to Death', author:'Neil Postman'}]
         };
     },
 };
